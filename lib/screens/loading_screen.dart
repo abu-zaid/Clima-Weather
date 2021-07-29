0712034d8f8b44ab372b87eaf8b4eea632dcc727
Abu Zaid <iam.abuzaid@gmail.com>
2021-07-29 13:56:51 +0530

    Setup workflow and get user location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
-
+import 'package:geolocator/geolocator.dart';
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
-}
 
+
+}
 class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState(
+    );
+    GetLocation();
+  }
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    super.deactivate();
+  }
+  void GetLocation()async{
+    LocationPermission permission = await Geolocator.requestPermission();
+
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(permission);
+    print(position);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
     );
   }
 }
